d2c6ddea6ee634d394279719439beb296b2926e5
ram <sairampadala001@gmail.com>
2025-09-18 01:34:18 +0530

    added some code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
+// src/components/Navbar.jsx
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
-    <header className="navbar">
-      <div className="logo">💲 PayOneRupee</div>
-      <nav>
-        <a href="/">Home</a>
-        <a href="/contact">Contact</a>
+    <header className="navbar bg-black text-white px-8 py-4 flex justify-between items-center">
+      <div className="logo text-yellow-400 font-bold text-xl">
+        💲 <Link to="/" className="hover:text-yellow-400">PayOneRupee</Link>
+      </div>
+      <nav className="flex gap-6">
+        <Link to="/" className="hover:text-yellow-400">Home</Link>
+        <Link to="/contact" className="hover:text-yellow-400">Contact</Link>
       </nav>
     </header>
   );
